a5cbc23b6da6e338f5d41b564bad48e1c7c863d9
Bruno Alves <bruno6821@gmail.com>
2022-12-27 14:54:10 -0300

    Implementando a Lógica do Reducer

--- posts.js
+++ posts.js
@@ -27,7 +27,23 @@ const removePostAction = (id) => {
 
 // 3- REDUCERS
 
-const postReducer = (state = initialState, action) => {};
+const postReducer = (state = initialState, action) => {
+  if (action.type === "ADD_POST") {
+    return {
+      // Pegamos o array de post que está no state e desestruturamos os dados
+      // assim os dados do estado anterior permanecerão junto com os dados da Action passada
+      posts: [...state.posts, action.post],
+    };
+  } else if (action.type === "REMOVE_POST") {
+    return {
+      posts: state.posts.filter((post) => {
+        return post.id !== action.id;
+      }),
+    };
+  } else {
+    return state;
+  }
+};
 
 // 4- STORE
 
